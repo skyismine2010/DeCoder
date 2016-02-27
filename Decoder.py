40d5b0c389f404f2e13b81889e5331cd31bd1c18
linhao <skyismine2010@163.com>
2016-02-27 22:10:48 +0800

    0227提交，修改配置读取方式

--- Decoder.py
+++ Decoder.py
@@ -1,74 +1,62 @@
 from libDiameter import *
 import xml.etree.cElementTree as ET
 
+class Field():
+    def __init__(self, fieldElement):
+        self.typeStr = fieldElement.get("type")
+        self.valStr = fieldElement.text
+        self.trueVal = None
 
-def loadTCTemplete(str):
-    root = ET.parse(str).getroot()
-    for msgElement in root.findall("Msg"):
-        if msgElement.get("type") == "dim":
-            strRawCode = msgElement.findtext("RawCode")
-            objConverter = DimConverter(msgElement.get("ChangeField"), strRawCode, msgElement.get("pos"))
+    def typeConvert(self):
+        pass
+
+
+class ChangeField():
+    def __init__(self, changeFieldElement):
+        self.nameStr = changeFieldElement.get("name")
+        self.fieldList = []
+        for filedElement in changeFieldElement.findall("Field"):
+            self.fieldList.append(Field(filedElement))
+
+
+class Message():
+    def __init__(self, msgCfgElement):
+        self.typeStr = msgCfgElement.get("type")
+        self.nameStr = msgCfgElement.get("name")
+        self.RawCode = msgCfgElement.find("RawCode").text
+        self.changeFieldList = []
+        for changeFieldElement in msgCfgElement.findall("ChangeField"):
+            self.changeFieldList.append(ChangeField(changeFieldElement))
 
 
 
 class TestCaseXmlTPL():
     def __init__(self, pathStr):
         self.root = ET.parse(pathStr).getroot()
-
-        self.msgsList = []
-        self.readMsgTag()
-
-        self.changeFieldsDict = {}
-        self.readChangeFieldTag()
-
-
-    def readMsgTag(self):
-
+        self.msgList = []
         for msgTagElement in self.root.findall("Msg"):
-            msgList = []
-            msgList.append(msgTagElement.get("name"))
-            msgList.append(msgTagElement.get("type"))
-            msgList.append(msgTagElement.find("RawCode").text)
-
-            changeFieldList =[]
-            for changeFieldTag in msgTagElement.findall("ChangeFieldName"):
-                changeFieldList.append(changeFieldTag.text)
-
-            msgList.append(changeFieldList)
-
-        self.msgsList.append(msgList)
+            self.msgList.append(Message(msgTagElement))
 
 
-
-
-    def readChangeFieldTag(self):
-        for cFTagElement in self.root.findall("ChangeField"):
-            fieldList = []
-            for field in cFTagElement.findall("Feild"):
-                fieldDict = {}
-                fieldDict["type"] = field.get("type")
-                fieldDict["field"] = field.text
-                fieldList.append(fieldDict)
-            self.changeFieldsDict[cFTagElement.get("name")] = fieldList
-
-
-class DimConverter():
-    def __init__(self,strChangeField, strRawCode, strPos):
-        self.strChangeField = strChangeField
-        self.strRawCode = strRawCode
-        self.strPos = strPos
-        self.ObjH = HDRItem()
-        self.listAVPs = []
-        self.rawCode2Struct(self)
-
-    def rawCode2Struct(self):
-        stripHdr(self.ObjH, self.strRawCode)
-        self.listAVPs = splitMsgAVPs(self.ObjH.msg)
+# class DimConverter():
+#     def __init__(self,msgObj):
+#         self.ObjH = HDRItem()
+#         self.listAVPs = []
+#         self.rawCode2Struct(self)
+#
+#     def rawCode2Struct(self):
+#         stripHdr(self.ObjH, self.strRawCode)
+#         self.listAVPs = splitMsgAVPs(self.ObjH.msg)
+#
+#     def converter(self):
+#         pass
 
 
 
-
-if __name__ =="__main__":
+if __name__ == '__main__':
+    LoadDictionary("dictDiameter.xml")
+    tcXmlTplObj = TestCaseXmlTPL("TCTemplete.xml")
+    print(tcXmlTplObj)
     # LoadDictionary("dictDiameter.xml")
     # msg="0100008c8000010100000000000000010860000100000108400000167374612e737072696e742e636f6d00000000012840000012737072696e742e636f6d0000000001014000000e00010a1e60c800000000010a4000000c000028af0000010d000000154c616e64736c69646548534757000000000001024000000c01000022000001164000000c4f32a086"
     # print "="*30
@@ -86,8 +74,7 @@ if __name__ =="__main__":
     #   print "Decoded AVP",decodeAVP(avp)
     # print "-"*30
 
-    loadTCTemplete("TCTemplete.xml")
-    tcXmlTplObj = TestCaseXmlTPL("TCTemplete.xml")
+
 
 
 
